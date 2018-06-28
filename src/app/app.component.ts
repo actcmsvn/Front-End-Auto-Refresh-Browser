f5f9e527d9a07d837a94bc15621ccab55ad9597f
Minh Long <hominhlong.it@gmail.com>
2018-06-28 16:21:43 +0700

    Add version checker service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from './../environments/environment';
+import { CheckerService } from './checker.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  version = environment.version;
+
+  constructor(
+    private checkerServ: CheckerService,
+  ) {
+  }
+
+  ngOnInit() {
+    // Tự động refresh browser khi deploy version mới!
+    this.checkerServ.version();
+  }
 }
