7c0a1c9d026cd737fffa3aaa320af27d8292a4d3
Minh Long <hominhlong.it@gmail.com>
2018-06-29 10:51:09 +0700

    Update deploy

--- src/environments/env.default.ts
+++ src/environments/env.default.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  versionCheckURL: '/version.json',
-  version: '1.0.001',
+  versionCheckURL: 'version.json',
+  version: '1.0.002',
 
   // Auto check and refresh browser after 60 seconds
   versionFrequency: 1000 * 60,
